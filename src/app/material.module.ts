3c6b47723dca50d276ea6b4ce74498910a44e1a6
Webad665 <114430012+Webad665@users.noreply.github.com>
2023-07-22 20:03:07 +0200

    sidenav

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -8,6 +8,8 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatNativeDateModule } from "@angular/material/core";
 import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
 
@@ -19,7 +21,9 @@ import { MatCheckboxModule } from "@angular/material/checkbox";
          MatFormFieldModule,
          MatDatepickerModule,
          MatNativeDateModule,
-         MatCheckboxModule
+         MatCheckboxModule,
+         MatSidenavModule,
+         MatToolbarModule
         ],
     exports: 
         [
@@ -29,7 +33,9 @@ import { MatCheckboxModule } from "@angular/material/checkbox";
          MatFormFieldModule,
          MatDatepickerModule,
          MatNativeDateModule,
-         MatCheckboxModule
+         MatCheckboxModule,
+         MatSidenavModule,
+         MatToolbarModule
         ]
 
 })
